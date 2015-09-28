49fcae3de03300f57f0a2fa0e5025c21efbc4dfe
rastko <rssole@gmail.com>
2015-09-29 00:36:46 +0200

    Added demo code for transactions

--- src/voxxed/concurrent.clj
+++ src/voxxed/concurrent.clj
@@ -1,21 +1,56 @@
 (ns voxxed.concurrent
   (:use [voxxed.support.concurrent :only [dothreads!]]))
 
-(def thread-ids-senders (atom #{}))
-(def thread-ids-receivers (atom #{}))
+(def thread-ids (agent {::senders #{} ::receivers #{}}))
 
 (def oo7 (agent 0))
 
-(defn- register-tid-in [atom]
-  (swap! atom conj (.getId (Thread/currentThread))))
+(defn- register-tid-in [loc tid]
+  (send thread-ids
+        #(update %
+                 loc
+                 (fn [s x]
+                   (conj s x)) tid)))
 
-(dothreads! #(do (register-tid-in thread-ids-senders)
+(dothreads! #(do (register-tid-in ::senders (.getId (Thread/currentThread)))
                  (send oo7 (fn [a]
-                             (register-tid-in thread-ids-receivers)
+                             (register-tid-in ::receivers (.getId (Thread/currentThread)))
                              (inc a)))) :times 30 :threads 4)
-; issue above: parallel work of agents serialized on atom
 
 (println "So far reached value of oo7:" @oo7)
+
+;(add-watch oo7 :m (fn [k r old new]
+;                    (when (= new 100)
+;                      (println "Turned 100"))))
+
+(def account-x (ref {:id 123 :balance 400}))
+(def account-y (ref {:id 234 :balance 500}))
+
+(defn transfer [amount src tgt]
+  (dosync
+    (alter src #(do
+                 ;(Thread/sleep 1500)
+                 ;(println "Taking money...")
+                 (update % :balance - %2)) amount)
+    (alter tgt #(do
+                 ;(Thread/sleep 1500)
+                 ;(println "Paying money...")
+                 (update % :balance + %2)) amount)))
+
+;(do
+;  (dothreads!
+;    (fn [] (send oo7 (fn [_ amount]
+;                       (Thread/sleep 500)
+;                       (println "Here is some money for laundering...")
+;                       (dosync
+;                         (alter account-x #(update % :balance + %2) amount))) 100))
+;    :times 2
+;    :threads 5)
+;  (transfer 500 account-x account-y))
+
+
+
+
 
 
 
--- src/voxxed/support/concurrent.clj
+++ src/voxxed/support/concurrent.clj
@@ -1,7 +1,7 @@
 (ns voxxed.support.concurrent
   (:import [java.util.concurrent Executors]))
 
-;Copyright (c) Fogus & Houser
+;Copyright (c) Fogus & Houser JOC 2nd edition
 ;
 (def thread-pool (Executors/newFixedThreadPool
                    (+ 2 (.availableProcessors (Runtime/getRuntime)))))
@@ -9,5 +9,5 @@
 (defn dothreads! [f & {thread-count :threads
                        exec-count :times
                        :or {thread-count 1 exec-count 1}}]
-  (dotimes [t thread-count]
+  (dotimes [_ thread-count]
     (.submit thread-pool #(dotimes [_ exec-count] (f)))))
