acc18e754fb6279edc8505ef1b1a6bc1bfa852ac
rastko <rssole@gmail.com>
2015-09-29 12:17:28 +0200

    Macros

--- src/voxxed/macros.clj
+++ src/voxxed/macros.clj
@@ -1,1 +1,17 @@
 (ns voxxed.macros)
+
+(defmacro foreach [[sym coll] & body]
+  `(loop [coll# ~coll]
+     (when-let [[~sym & xs#] (seq coll#)]
+       ~@body
+       (recur xs#))))
+
+(macroexpand
+  '(foreach [ch ["Here is a simple macro"]]
+           (println ch)))
+
+(macroexpand
+  '(->> "Here is another one but much more useful"
+       (filter #(not (#{\a \e \i \o \u} %)))
+       (map #(Character/toUpperCase %))
+       (apply str)))
